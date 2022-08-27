66cd8d528367b44a513918e2d8f93cf23baad4d8
RaulFR01 <rferrazrobles@hotmail.com>
2022-08-27 14:38:19 +0200

    iteration 2 first try

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <NavBar />
+  <p>The result of the operation is {{ result }}</p>
   <FooterSection />
 </template>
 
@@ -8,9 +9,18 @@ import FooterSection from "./components/footerSection.vue";
 import NavBar from "./components/navBar.vue";
 export default {
   name: "App",
+  data: function () {
+    return {
+      result: 0,
+    };
+  },
   components: {
     FooterSection,
     NavBar,
+  },
+
+  created() {
+    this.result = 2 + 2;
   },
 };
 </script>
