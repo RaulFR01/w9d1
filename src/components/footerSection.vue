2ddcda3d06a7335e5206e37805fa19c2e14ecb16
RaulFR01 <rferrazrobles@hotmail.com>
2022-08-27 14:17:38 +0200

    iteration 1 done

---
+++ src/components/footerSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <h2>This is the footer</h2>
+    <ul>
+      <li>Home</li>
+      <li>Project</li>
+      <li>Team</li>
+      <li>About us</li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footerSection",
+};
+</script>
+
+<style></style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <nav>
+      <h1>This is the navBaR</h1>
+      <h1>Circle</h1>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>Services</li>
+      </ul>
+    </nav>
+  </header>
+  <hr />
+</template>
+
+<script>
+export default {
+  name: "navBar",
+};
+</script>
+
+<style></style>
