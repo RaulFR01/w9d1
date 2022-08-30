aba57898dad927e0031a5f42edf8d52694e22bfe
RaulFR01 <rferrazrobles@hotmail.com>
2022-08-30 21:09:53 +0200

    bonus w9d1 done

--- src/components/formBonus.vue
+++ src/components/formBonus.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="bonus">
     <p><b>This is my form</b></p>
-    <form action="">
+    <form action="" @submit.prevent="alertForm">
       <fieldset>
         <input type="text" v-model="name" />
       </fieldset>
       <fieldset>
         <input type="mail" v-model="email" />
       </fieldset>
+      <input type="submit" />
     </form>
 
     <p><b>This is the two-way-data-binding result</b></p>
@@ -25,6 +26,11 @@ export default {
       name: "",
       email: "",
     };
+  },
+  methods: {
+    alertForm() {
+      alert("Error connecting to the server");
+    },
   },
 };
 </script>
